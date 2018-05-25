c75bb2d9c9de86d774964683d04f15f5bc2a3e39
Kohei Hakoishi <wipiano@gmail.com>
2018-05-26 01:46:16 +0900

    abstract Token class

---
+++ Stone/StoneException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Stone
+{
+    public class StoneException : Exception
+    {
+        public StoneException(string message)
+            : base(message)
+        {
+        }
+        
+        // TODO: support ASTree
+    }
+}
---
+++ Stone/Token.cs
@@ -0,0 +1,22 @@
+﻿namespace Stone
+{
+    public abstract class Token
+    {
+        public int LineNumber { get; }
+        
+        protected Token(int lineNumber)
+        {
+            LineNumber = lineNumber;
+        }
+
+        public virtual bool IsIdentifier => false;
+
+        public virtual bool IsNumber => false;
+
+        public virtual bool IsString => false;
+        
+        public virtual int GetNumber() => throw new StoneException("not number token");
+
+        public virtual string GetText() => string.Empty;
+    }
+}
